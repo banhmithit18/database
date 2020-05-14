c1d1fe596ac6fe45ea05cde03cf8c9547cb4d88a
K <khoadh28@gmail.com>
2020-05-14 09:18:53 +0700

    data

--- Tables/StaticData/dbo.crime.Data.sql
+++ Tables/StaticData/dbo.crime.Data.sql
@@ -4,6 +4,7 @@ GO
 INSERT dbo.crime(crimeid, crimename) VALUES (1, N'Gi?t ngu?i')
 INSERT dbo.crime(crimeid, crimename) VALUES (2, N'Ch?ng ngu?i thi hành công v?')
 INSERT dbo.crime(crimeid, crimename) VALUES (3, N'Tàng tr? trái phép súng')
+
 GO
 SET IDENTITY_INSERT dbo.crime OFF
 GO
--- Tables/StaticData/dbo.prisoner.Data.sql
+++ Tables/StaticData/dbo.prisoner.Data.sql
@@ -2,6 +2,7 @@
 SET IDENTITY_INSERT dbo.prisoner ON
 GO
 INSERT dbo.prisoner(prisonerid, prisonername, prisonerage, gender, dateofbirth, dateofarrest, crime, punishment, cellroom, address, city, country) VALUES (1, N'Ðang Khoa', 18, N'Nam', '2001-01-01 00:00:00.000', '2020-05-09 00:00:00.000', 1, 1, 2, N'33 BDT', 1, 1)
+
 GO
 SET IDENTITY_INSERT dbo.prisoner OFF
 GO
